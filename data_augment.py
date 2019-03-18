7638a9111974bd248ce4c4a034cd5ea8bb7626e2
yama <maiden635@gmail.com>
2019-03-19 01:03:02 +0900

    [refactoring] data_augment.py
    コメント文のIDE対応

--- data_augment.py
+++ data_augment.py
@@ -1,16 +1,62 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-#
-# usage: ./increase_picture.py hogehoge.jpg
-#
+
+# 画像データの増強・水増しをopencvのエフェクトを使って行う
 
 import cv2
 import numpy as np
-import sys
+# import sys
 import os
 
 
+def data_augment(newfolder, file, src0, num_list, mode):
+    '''
+    newfolderは保存先のフォルダ
+    fileは元々のfile名（拡張子なし）→のちにjpgをたす
+    srcは元の画像（numpy array型式）
+    num_listはどの変換を適応するか
+    modeはflipのどれを含めるか（2018/4/1時点では左右flipのみ）
+    '''
+
+    data = Trans()
+    for num in num_list:
+        if mode == 0:
+            dst0 = data.convert(src0, num)
+            newfile0 = str(num) + "_" + file + ".jpg"
+            newfpath0 = os.path.join(newfolder, newfile0)
+            # 全ての画像に同じ処理をしたい場合はここに！マスキングなど
+            cv2.imwrite(newfpath0, dst0)
+        if mode == 1:
+            src1 = data.lr_flip_func(src0)
+            dst0 = data.convert(src0, num)
+            dst1 = data.convert(src1, num)
+            newfile0 = "0" + "_" + str(num) + "_" + file + ".jpg"
+            newfile1 = "1" + "_" + str(num) + "_" + file + ".jpg"
+            newfpath0 = os.path.join(newfolder, newfile0)
+            newfpath1 = os.path.join(newfolder, newfile1)
+            cv2.imwrite(newfpath0, dst0)
+            cv2.imwrite(newfpath1, dst1)
+
+
 class Trans:
+    '''
+    Transクラスで具体的な処理を記述
+        ①そのまま、 nothing
+        ②High contrast  high_contrast1
+        ③Low contrast   low_contrast1
+        ④Gamma変換1     gamma1
+        ⑤Gamma変換2     gamma2
+        ⑥平滑化         blur
+        ⑦ヒストグラム均一化     equalizeHistRGB
+        ⑧ガウシアンノイズ       addGaussianNoise
+        ⑨ソルトペッパーノイズ   addSaltPepperNoise
+        ⑩左右反転               lr_flip_func
+
+    data_augment関数内における処理の呼び出し
+        data = Trans()
+        処理後画像 = data.convert(処理前画像, 処理に対応する数字)
+    '''
+
     def __init__(self):
         # ルックアップテーブルの生成
         self.min_table = 50
@@ -18,6 +64,7 @@ class Trans:
         self.diff_table = self.max_table - self.min_table
         self.gamma1 = 0.75
         self.gamma2 = 1.5
+
         # 平滑化用
         self.average_square = (10, 10)
 
@@ -48,8 +95,10 @@ class Trans:
         self.LUTs.append(self.LUT_G1)
         self.LUTs.append(self.LUT_G2)
 
-        self.func_list = [self.nothing_func, self.hc1_func, self.lc1_func, self.gamma1_func, self.gamma2_func,
-                          self.blur_func, self.equalizeHistRGB_func, self.addGaussianNoise_func, self.addSaltPepperNoise_func]
+        self.func_list = [self.nothing_func, self.hc1_func, self.lc1_func,
+                          self.gamma1_func, self.gamma2_func, self.blur_func,
+                          self.equalizeHistRGB_func, self.addGaussianNoise_func,
+                          self.addSaltPepperNoise_func, self.lr_flip_func]
 
     # ① 何もしない
     def nothing_func(self, src):
@@ -59,40 +108,39 @@ class Trans:
     def hc1_func(self, src):
         img_dst = cv2.LUT(src, self.LUTs[0])
         return img_dst
-    # ③ low_contrast1
 
+    # ③ low_contrast1
     def lc1_func(self, src):
         img_dst = cv2.LUT(src, self.LUTs[1])
         return img_dst
-    # ④ gamma補正1
 
+    # ④ gamma補正1
     def gamma1_func(self, src):
         img_dst = cv2.LUT(src, self.LUTs[2])
         return img_dst
-    # ⑤ gamma補正2
 
+    # ⑤ gamma補正2
     def gamma2_func(self, src):
         img_dst = cv2.LUT(src, self.LUTs[3])
         return img_dst
-    # ⑥　平滑化
 
+    # ⑥ 平滑化
     def blur_func(self, src):
         img_dst = cv2.blur(src, self.average_square)
         return img_dst
-    # ⑦ヒストグラム均一化
-    def equalizeHistRGB_func(self, src):
 
+    # ⑦ ヒストグラム均一化
+    def equalizeHistRGB_func(self, src):
         RGB = cv2.split(src)
         Blue = RGB[0]
         Green = RGB[1]
         Red = RGB[2]
         for i in range(3):
             cv2.equalizeHist(RGB[i])
-
         img_hist = cv2.merge([RGB[0], RGB[1], RGB[2]])
         return img_hist
-    # ⑧ガウシアンノイズ
 
+    # ⑧ ガウシアンノイズ
     def addGaussianNoise_func(self, src):
         row, col, ch = src.shape
         mean = 0
@@ -101,15 +149,15 @@ class Trans:
         gauss = np.random.normal(mean, sigma, (row, col, ch))
         gauss = gauss.reshape(row, col, ch)
         noisy = src + gauss
-
         return noisy
-    # ⑨salt&pepperノイズ
 
+    # ⑨ salt&pepperノイズ
     def addSaltPepperNoise_func(self, src):
         row, col, ch = src.shape
         s_vs_p = 0.5
         amount = 0.004
         out = src.copy()
+
         # Salt mode
         num_salt = np.ceil(amount * src.size * s_vs_p)
         coords = [np.random.randint(0, i-1, int(num_salt))
@@ -122,52 +170,12 @@ class Trans:
                   for i in src.shape]
         out[coords[:-1]] = (0, 0, 0)
         return out
-    # ⑩反対
 
+    # ⑩ 反対
     def lr_flip_func(self, src):
         img_dst = cv2.flip(src, 1)
         return img_dst
 
-    # ↑が関数の具体的定義
-    # ①そのまま、 nothing
-    # LUTsに含まれる
-    # ②High contrast   high_contrast1
-    # ③Low contrast    low_contrast1
-    # ④Gamma変換1,     gamma1
-    # ⑤Gamma変換2      gamma2
-    # ⑥平滑化          blur
-    # ⑦ヒストグラム均一化    equalizeHistRGB
-    # ⑧ガウシアンノイズ      addGaussianNoise
-    # ⑨ソルトペッパーノイズ  addSaltPepperNoise
-    # 下が関数のリスト
-
     def convert(self, src, num):
         img_dst = self.func_list[num](src)
         return img_dst
-
-# newfolderは保存先のフォルダ
-# fileは元々のfile名（拡張子なし）→のちにjpgをたす
-# srcは元の画像（numpy array型式）
-# num_listはどの変換を適応するか
-# modeはflipのどれを含めるか（2018/4/1時点では左右flipのみ）
-
-
-def data_augment(newfolder, file, src0, num_list, mode):
-    data = Trans()
-    for num in num_list:
-        if mode == 0:
-            dst0 = data.convert(src0, num)
-            newfile0 = str(num) + "_" + file + ".jpg"
-            newfpath0 = os.path.join(newfolder, newfile0)
-            # 全ての画像に同じ処理をしたい場合はここに！マスキングなど
-            cv2.imwrite(newfpath0, dst0)
-        if mode == 1:
-            src1 = data.lr_flip_func(src0)
-            dst0 = data.convert(src0, num)
-            dst1 = data.convert(src1, num)
-            newfile0 = "0" + "_" + str(num) + "_" + file + ".jpg"
-            newfile1 = "1" + "_" + str(num) + "_" + file + ".jpg"
-            newfpath0 = os.path.join(newfolder, newfile0)
-            newfpath1 = os.path.join(newfolder, newfile1)
-            cv2.imwrite(newfpath0, dst0)
-            cv2.imwrite(newfpath1, dst1)
