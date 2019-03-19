12bba5f651ea6ab0464baa926f2dc9ecb9cf044b
yama <maiden635@gmail.com>
2019-03-19 18:33:35 +0900

    [update] validation_data.py
    コメントのIDE対応
    不要importコメントアウト

--- validation_data.py
+++ validation_data.py
@@ -1,27 +1,24 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+# 評価用データの作成および読みこみ
+
 import os
-import sys
+# import sys
 import pandas as pd
 import shutil
 import numpy as np
-import cv2
-import random
-from keras.utils import np_utils
+# import cv2
+# import random
+# from keras.utils import np_utils
 from utils import folder_create, fpath_tag_making, read_img
 
-# program.validation_data
-# Date: 2018/05/21
-# Filename: validation_data
-# To change this template, choose Tools | Templates
-# and open the template in the editor.
-__author__ = 'masuo'
-__date__ = "2018/05/21"
 
-
-# 評価用データの作成および読み込みのクラス
 class Validation(object):
+    '''
+    評価用データの作成および読み込みのクラス
+    '''
+
     def __init__(self, size, source_folder, test_root, dataset_folder, classes, pic_mode, idx):
         self.source_folder = source_folder
         self.test_root = test_root
@@ -34,11 +31,15 @@ class Validation(object):
         self.w = self.size[1]
         self.random_seed = 1
 
-    # 評価用データの画像を出力（画像解析用、カテゴリー分類）
     def pic_df_test(self):
+        '''
+        評価用データの画像を出力（画像解析用、カテゴリー分類）
+        '''
+
         df_test = pd.read_csv(os.path.join(
             self.dataset_folder, "test" + "_" + str(self.idx) + "." + "csv"), encoding="utf-8")
         columns = df_test.columns
+
         # test作成
         folder_create(self.test_root)
         for column in columns:
@@ -54,9 +55,11 @@ class Validation(object):
                 shutil.copy(img_path, new_path)
         return
 
-    # 評価用データ生成（画像からtest/00_normal/画像となっている想定）
-
     def pic_gen_data(self):
+        '''
+        評価用データ生成（画像からtest/00_normal/画像となっている想定）
+        '''
+
         fpath_list, tag_array = fpath_tag_making(self.test_root, self.classes)
         X_val = []
         y_val = []
@@ -65,11 +68,13 @@ class Validation(object):
             X_val.append(X)
             if(self.pic_mode == 2):
                 y_val.append(int(fpath.split("\\")[-1].split("_")[0]))
+
         # 全てを再度array化する
         X_val = np.array(X_val)
         if(self.pic_mode == 2):
             y_val = np.array(y_val)
-            # class数がいくつか
+
+        # class数がいくつか
         print("%d classes" % self.classes)
         # validation dataがいくつか。
         print("test_data = %d" % (len(X_val)))
