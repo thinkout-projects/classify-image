684ac7ddd03eab86f24c8879ae1aad889db6c6bc
yama <maiden635@gmail.com>
2019-03-23 01:24:28 +0900

    [update] training_data.py
    コメントのIDE対応
    不要importコメントアウト
    100文字/行 収め

--- training_data.py
+++ training_data.py
@@ -8,18 +8,12 @@ import cv2
 import random
 import threading
 from data_augment import data_augment
-from utils import folder_create, fpath_tag_making, read_img
+from utils import folder_create, read_img
+# from utils import fpath_tag_making
 from keras.preprocessing.image import ImageDataGenerator
-from keras.utils import np_utils
+# from keras.utils import np_utils
 
-
-# Projects.pic_data_augment
-# Date: 2018/04/02
-# Filename: pic_data_augment
-# To change this template, choose Tools | Templates
-# and open the template in the editor.
-__author__ = 'masuo'
-__date__ = "2018/04/02"
+# 訓練用データ(= trainフォルダ)の準備
 
 # k_fold_split(k,img_root,dataset_folder)
 # train_0.csvなどとなる。columnsはimg_rootにあるフォルダ名
@@ -57,8 +51,9 @@ def threadsafe_generator(f):
 
 
 class Training(object):
-    def __init__(self, source_folder, dataset_folder, train_root, idx, pic_mode, train_num_mode_dic, size, classes,
-                 rotation_range, width_shift_range, height_shift_range, shear_range, zoom_range, BATCH_SIZE):
+    def __init__(self, source_folder, dataset_folder, train_root, idx, pic_mode,
+                 train_num_mode_dic, size, classes, rotation_range,
+                 width_shift_range, height_shift_range, shear_range, zoom_range, BATCH_SIZE):
         self.source_folder = source_folder
         self.train_root = train_root
         self.idx = idx
@@ -76,11 +71,14 @@ class Training(object):
         self.zoom_range = zoom_range
         self.BATCH_SIZE = BATCH_SIZE
 
-    # Datasetから訓練用フォルダを作成
     def pic_df_training(self):
+        '''
+        Datasetから訓練用フォルダを作成
+        '''
         df_train = pd.read_csv(os.path.join(
             self.dataset_folder, "train" + "_" + str(self.idx) + "." + "csv"), encoding="utf-8")
         columns = df_train.columns
+
         # train作成
         folder_create(self.train_root)
         for column in columns:
@@ -88,13 +86,16 @@ class Training(object):
             train_folder = os.path.join(self.train_root, column)
             folder_create(train_folder)
             train_list = df_train[column].dropna()
+
             # 画像ごとに
             for train_file in train_list:
                 print(train_file)
+
                 # img/00_normal/画像
                 img_path = os.path.join(self.source_folder, column, train_file)
                 src0 = cv2.imread(img_path)
                 file_without = train_file.split(".")[0]
+
                 # train_num_mode_dicはフォルダ名をKeyとして、numとmodeのリストを保持している。
                 # num・・・9個の変換の中で指定の数だけを作成する。
                 # mode・・・0なら左右反転なし、1なら左右反転あり。
@@ -105,11 +106,13 @@ class Training(object):
                 data_augment(train_folder, file_without, src0, num_list, mode)
         return
 
-    # さらにnumpyの中でデータ拡張を行うもの。
     def data_gen(self, X_train):
-        # ImageDataGeneratorは設定した後、fitして使うもの
-        # fitの引数はX_train
-        # 代入しなくても、fitするだけでrandomにX_trainの中身をいじってくる。
+        '''
+        さらにnumpyの中でデータ拡張を行うもの
+        ImageDataGeneratorは設定した後、`fit`して使うもの
+        `fit`の引数は`X_train`
+        代入しなくても、`fit`するだけでrandomに`X_train`の中身をいじってくる。
+        '''
         IDG = ImageDataGenerator(
             # rescale=1./255,
             rotation_range=self.rotation_range,
