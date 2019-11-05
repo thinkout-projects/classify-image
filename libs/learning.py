a4e0fe6e03234883fc2e53e453ecf887a2190ef7
burokoron <sapatapata831@gmail.com>
2019-11-05 20:50:33 +0900

    update：学習を行いながらdata augmentationするように変更

--- libs/learning.py
+++ libs/learning.py
@@ -2,6 +2,8 @@
 # -*- coding: utf-8 -*-
 
 import os
+import dataclasses
+import numpy as np
 from .utils.folder import folder_create
 
 # plot用に
@@ -10,33 +12,53 @@ import matplotlib.pyplot as plt
 # main関数
 from tensorflow.keras.callbacks import ModelCheckpoint, ReduceLROnPlateau
 from tensorflow.keras.callbacks import EarlyStopping
-from .data_generator import Training
-from .utils.utils import fpath_tag_making
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tqdm import tqdm
+
+from .data_generator import Trans
+from .utils.utils import read_img
 
 
 # モニターのないサーバーでも動作するように
 plt.switch_backend('agg')
 
 
-class Learning(Training):
-    '''
-    Trainingのクラスをスーパークラスとして、サブクラスである学習クラスを作成
-    '''
+@dataclasses.dataclass
+class Learning:
+    """
+    ニューラルネットワークモデルの学習を行うときのパラメータなどを保持する
+    """
 
-    def __init__(self, folder_names, idx,
-                 pic_mode, train_num_mode_dic, size, classes, positive_label,
-                 args_of_IDG, BATCH_SIZE, model_folder, model,
-                 X_val, y_val, epochs, df_train):
-        super().__init__(folder_names, idx,
-                         pic_mode, train_num_mode_dic, size, classes,
-                         positive_label, args_of_IDG, BATCH_SIZE, df_train)
-        self.model_folder = model_folder
-        self.model = model
-        self.pic_mode = pic_mode
-        self.X_val = X_val
-        self.y_val = y_val
-        self.epochs = epochs
-        self.batch_size = BATCH_SIZE
+    directory: str # 画像が保存されているフォルダ
+    csv_config: dict # 学習に使用するデータの情報が書かれたcsvの情報
+    df_train: None # 学習データの情報がかかれたDataFrame
+    df_validation: None # 検証データの情報がかかれたDataFrame
+    label_list: list # ラベル名
+    idx: int # k-fold splitの何番目か
+    base_augmentation: dict # ライブラリを使用した画像増幅のパラメータ
+    extra_augmentation: dict # 自作の画像増幅のパラメータ
+    image_size: tuple # 入力画像サイズ
+    classes: int # 分類クラス数
+    batch_size: int # バッチサイズ
+    model_folder: str # モデルの重みを保存するフォルダ
+    epochs: int # エポック数
+
+    
+    def __post_init__(self):
+        self.filename_column = self.csv_config["image_filename_column"] # ファイル列
+        self.label_column = self.csv_config["label_column"] # ラベル列
+        self.group_column = self.csv_config["ID_column"] # グループ列
+        if self.classes == 1:
+            self.class_mode = 'other'
+        else:
+            self.class_mode = 'categorical'
+        for key in self.base_augmentation:
+            if (self.base_augmentation[key] != 'True'
+                and self.base_augmentation[key] != 'False'):
+                self.base_augmentation[key] = float(self.base_augmentation[key])
+            else:
+                self.base_augmentation[key] = bool(self.base_augmentation[key])
+
 
     def balance_making(self):
         balance_list = []
@@ -56,7 +78,8 @@ class Learning(Training):
         print(dic)
         return dic
 
-    def learning_model(self):
+
+    def train(self, model):
         # modelフォルダを作成
         folder_create(self.model_folder)
         model_file = "weights_" + str(self.idx) + "_epoch{epoch:02}.hdf5"
@@ -73,27 +96,69 @@ class Learning(Training):
 
         # 学習が進まなくなったら、強制的に学習終了
         es_cb = EarlyStopping(monitor='loss', min_delta=0,
-                              patience=10, verbose=1, mode='auto')
+                              patience=5, verbose=1, mode='auto')
 
-        # fpath_list,tag_listを作成する
-        fpath_list, tag_array = fpath_tag_making(self.train_root, self.classes,
-                                                 self.positive_label)
         # balance = Learning.balance_making(self)
 
+        # ジェネレータの生成
+        ## 学習データのジェネレータ
+        augmentor = Trans(**self.extra_augmentation)
+        datagen = ImageDataGenerator(
+            rescale=1./255,
+            preprocessing_function=augmentor.augment(),
+            **self.base_augmentation)
+        train_generator = datagen.flow_from_dataframe(
+            dataframe=self.df_train, directory=self.directory,
+            x_col=self.filename_column, y_col=self.label_column,
+            target_size=self.image_size, class_mode=self.class_mode, 
+            classes=self.label_list,
+            batch_size=self.batch_size)
+        step_size_train = train_generator.n // train_generator.batch_size
+        ## 検証データのジェネレータ
+        augmentor = Trans()
+        datagen = ImageDataGenerator(
+            rescale=1./255,
+            preprocessing_function=augmentor.augment())
+        validation_generator = datagen.flow_from_dataframe(
+            dataframe=self.df_validation, directory=self.directory,
+            x_col=self.filename_column, y_col=self.label_column,
+            target_size=self.image_size, class_mode=self.class_mode, 
+            classes=self.label_list,
+            batch_size=self.batch_size)
+        step_size_validation = \
+            validation_generator.n // validation_generator.batch_size
+
         # 実際に学習⇒historyを作成
-        history = self.model.fit_generator(Learning.datagen(
-                                           self, fpath_list=fpath_list,
-                                           tag_array=tag_array),
-                                           int(len(fpath_list) /
-                                               self.batch_size),
-                                           epochs=self.epochs,
-                                           # class_weight = balance,
-                                           validation_data=(
-            self.X_val, self.y_val),
-            callbacks=[mc_cb, rl_cb, es_cb],
-            workers=1,
-            verbose=1)
+        history = model.fit_generator(
+            train_generator, steps_per_epoch=step_size_train,
+            epochs=self.epochs, verbose=1, callbacks=[mc_cb, rl_cb, es_cb],
+            validation_data=validation_generator,
+            validation_steps=step_size_validation,
+            # class_weight = balance,
+            workers=3)
+
+
         return history
+
+
+    def predict(self, model):
+        y_pred = []
+        W_val = self.df_validation[self.filename_column].values
+        if self.classes != 1:
+            y_val = list(map(lambda x: self.label_list.index(x),
+                             self.df_validation[self.label_column].values))
+        else:
+            y_val = self.df_validation[self.label_column].values.astype(float)
+        for file in tqdm(W_val, desc='predict_validation'):
+            image = read_img(os.path.join(self.directory, file),
+                             self.image_size[0], self.image_size[1])
+            image = np.expand_dims(image, axis=0)
+            if self.classes != 1:
+                y_pred.append(model.predict(image)[0])
+            else:
+                y_pred.append(model.predict(image)[0][0])
+
+        return W_val, y_val, y_pred
 
 
 def plot_hist(history, history_folder, metrics, idx):
